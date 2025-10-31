bf3add1364f234cd8ab49b4888b54ba6e8ffeee4
sandulr <sandulr2@gmail.com>
2025-11-01 01:25:44 +0530

    Add files via upload

---
+++ app/routes/auth/login.jsx
@@ -0,0 +1,116 @@
+import { useActionData, json, redirect } from 'remix';
+import {db} from '~/utils/db.server';
+import {login, register, createUserSession} from '~/utils/session.server';
+
+function badRequest(data){
+    return json(data, {status: 400})
+}
+
+function validateUsername(username){
+    if(typeof username !== 'string' || username.length < 4){
+        return 'Username should be at least 4 characters long'
+    }
+}
+
+function validatePassword(password){
+    if(typeof password !== 'string' || password.length < 6){
+        return 'Password should be at least 6 characters long'
+    }
+}
+
+export const action = async ({request}) => {
+    const form = await request.formData()
+    const loginType = form.get('loginType')
+    const username = form.get('username')
+    const password = form.get('password')
+
+    const fields = {loginType, username, password}
+
+    const fieldErrors = {
+        username:validateUsername(username),
+        password:validatePassword(password)
+    }
+
+    if(Object.values(fieldErrors).some(Boolean)){
+        return badRequest({fieldErrors, fields})
+    }
+
+    switch(loginType){
+        case 'login':{
+            const user = await login({username, password})
+
+            if(!user){
+                return badRequest({fields, fieldErrors:{username:'Invalid credentials'}})
+            }
+
+            return createUserSession(user.id, '/posts/')
+        }
+        case 'register':{
+            const userExists = await db.user.findFirst({
+                where:{
+                    username
+                }
+            })
+
+            if(userExists){
+                return badRequest({fields, fieldErrors:{username:`User ${username} already exists`}})
+            }
+
+            const user = await register({username, password})
+
+            if(!user){
+                return badRequest({
+                    fields,
+                    formError:'Something went wrong'
+                })
+            }
+
+            return createUserSession(user.id, '/posts/')
+        }
+        default:{
+            return badRequest({fields, formError:'Login type is not valid',})
+        }
+    }
+}
+
+function Login() {
+    const actionData = useActionData()
+
+    return (
+        <div className="auth-container">
+            <div className="page-header">
+                <h1>Login</h1>
+            </div>
+            <div className="page-content">
+                <form method="POST">
+                    <fieldset>
+                        <legend>Login or Register</legend>
+                        <label>
+                            <input type="radio" name="loginType" value='login' defaultChecked={!actionData?.fields?.loginType || actionData?.fields?.loginType === 'login'} /> Login
+                        </label>
+                        <label>
+                            <input type="radio" name="loginType" value='register' /> Register
+                        </label>
+                    </fieldset>
+                    <div className="form-control">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" name="username" id='username' defaultValue={actionData?.fields?.username && actionData?.fields?.username} placeholder="Username" />
+                        <div className="error">
+                            {actionData?.fieldErrors?.username && actionData?.fieldErrors?.username}
+                        </div>
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" id='password' defaultValue={actionData?.fields?.password && actionData?.fields?.password} placeholder="Password" />
+                        <div className="error">
+                            {actionData?.fieldErrors?.password && actionData?.fieldErrors?.password}
+                        </div>
+                    </div>
+                    <button className="btn btn-block" type="submit">Login</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login
